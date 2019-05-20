a5ea75cb78d13cd7fddc6bb9c7c8bbfd8f4cec84
Karla Esquivel <karla.eh11@gmail.com>
2019-05-20 08:43:15 -0700

    Ejercicios JS
    Signed-off-by: Karla Esquivel <karla.eh11@gmail.com>

--- workHere.js
+++ workHere.js
@@ -4,7 +4,14 @@ var billionaires = require('./billionaires.json');
     Get the names of the billionaires that are in their 20s
 */
 
-let twentyYearOldBillionaires = null;
+let twentyYearOldBillionaires = billionaires.filter( x => {
+    if (x.demographics.age > 19 && x.demographics.age < 30){
+        return x.name;
+    };
+});
+
+
+
 
 /*
     create a new object with the billionairse year, name and worth rounded to the newarest integer
@@ -15,38 +22,52 @@ let twentyYearOldBillionaires = null;
     }
 */
 
-let roundedWorth = null;
+let roundedWorth = billionaires.map (x => {
+    return {
+        year: x.year,
+        name: x.name,
+        worth: Math.round(x.wealth["worth in billions"])
+    };
+});
+
 
 /*
     sum the rounded worth of all the billionaires
-*/
+
 
 let aggregatedWorth = null;
-// console.log(aggregatedWorth)
+console.log(aggregatedWorth)
+
+*/
 
 /*
     find the the richest billionaire, make sure the result is in an array
 */
 
-let richest = null
+let richest = arr.ForEach (x => {
+        if (x.rank === 1){
+            console.log(x.name);
+        }
+    })
+}
 
 /*
     find the poorest billioanire, make sure the result is in an array
 */
 
-let poorest = null;
+//let poorest = null;
 
 /*
     make a new array containing the richest and the poorest billionaire
 */
 
-let richAndPoor = null;
+//let richAndPoor = null;
 
 module.exports = {
     twentyYearOldBillionaires,
     roundedWorth,
-    aggregatedWorth,
+    //aggregatedWorth,
     richest,
-    poorest,
-    richAndPoor
+    //poorest,
+    //richAndPoor
 }
